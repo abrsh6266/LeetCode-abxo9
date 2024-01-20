fe22a4b8e6f6a04b08d97d7aab5f0e68c2f2bada
abrsh6266 <118188730+abrsh6266@users.noreply.github.com>
2024-01-20 02:22:19 -0900

    Time: 65 ms (68.10%) | Memory: 16.7 MB (55.56%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,11 +1,30 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1 = list(s1)
-        s1.sort()
-        l = len(s1)
-        for i in range(len(s2)-l+1):
-            req = list(s2[i:i+l])
-            req.sort()
-            if req == s1:
-                return (True)
-        return False           
+        n = len(s1)
+        m = len(s2)
+        if n > m: return False
+        h1,h2 = [0] * 26, [0] * 26
+        for i in range(n):
+            h1[ord(s1[i]) - ord('a')] += 1
+            h2[ord(s2[i]) - ord('a')] += 1
+        match = 0
+        for i in range(26):
+            if h1[i] == h2[i]:
+                match += 1
+        l = 0
+        for r in range(n,m):
+            if match == 26: return True  
+            t = ord(s2[r])-ord('a')
+            h2[t] += 1
+            if h2[t] == h1[t]:
+                match +=1
+            elif h1[t] + 1 == h2[t]:
+                match -= 1
+            t = ord(s2[l])-ord('a')
+            h2[t] -= 1
+            if h2[t] == h1[t]:
+                match +=1
+            elif h2[t] + 1 == h1[t]:
+                match -= 1
+            l += 1
+        return match == 26
